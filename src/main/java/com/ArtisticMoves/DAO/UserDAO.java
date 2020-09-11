06a050512fa8808db5994693b546cb58c770e3b2
Sumeet Balwade <balwadesr@gmail.com>
2020-09-11 13:51:34 +0530

    added basic structure user
    basic user DAO and User Model created

---
+++ src/main/java/com/ArtisticMoves/DAO/UserDAO.java
@@ -0,0 +1,24 @@
+package com.ArtisticMoves.DAO;
+
+import com.ArtisticMoves.model.User;
+
+import java.sql.ResultSet;
+
+public class UserDAO {
+
+    //method to get user
+    public ResultSet getUser(){
+        ResultSet rs = null;
+        //create connection of data to get all user from user table
+        return rs;
+    }
+
+    //method to insert user
+    public int insertUser(User user){
+        int intergervalue = 0;
+       //create connection of data to insert user from user table
+        return intergervalue;
+    }
+
+
+}
---
+++ src/main/java/com/ArtisticMoves/model/User.java
@@ -0,0 +1,19 @@
+package com.ArtisticMoves.model;
+
+public class User {
+
+    //all private variable to create pojo class
+    private String name;
+//    add more variables
+
+
+
+//    create getter and setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
