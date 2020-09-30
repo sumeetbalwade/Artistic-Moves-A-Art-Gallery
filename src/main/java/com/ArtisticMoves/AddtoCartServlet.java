9ade8b8265c211628529550fcb569f1ac6b3569b
Sumeet Balwade <balwadesr@gmail.com>
2020-09-30 10:59:10 +0530

    cart designed

---
+++ src/main/java/com/ArtisticMoves/AddtoCartServlet.java
@@ -0,0 +1,22 @@
+package com.ArtisticMoves;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "AddtoCartServlet")
+public class AddtoCartServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession s = request.getSession(false);
+        if (s.getAttribute("user") != null) {
+
+        } else {
+            response.sendRedirect("Login.jsp");
+        }
+    }
+}
